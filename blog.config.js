992bafc8619ecf65f70579fff65b517bb858f9a0
ph0ven1x <96399817+ph0ven1x@users.noreply.github.com>
2024-12-25 11:38:05 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -27,7 +27,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'ph0ven1x', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://ph0ven1x.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '渗透测试', '安全研究' // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '渗透测试, 安全研究, 网络安全, Web安全, 红队, 攻防, 蓝队' // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
